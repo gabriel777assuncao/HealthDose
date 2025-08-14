5948118de81be9717d24e55df121b8714db47daf
gabriel assunção <gabriel.costa@sga.pucminas.br>
2025-08-13 21:58:59 -0300

    Feat: introducting rector to HealthDose backend

---
+++ src/.rector-cache/5f/87/5f8789986425206a3f9ef4d74dad24e25144b74b.php
@@ -0,0 +1,6 @@
+<?php declare(strict_types = 1);
+
+return \Rector\Caching\ValueObject\CacheItem::__set_state(array(
+   'variableKey' => 'file_hash',
+   'data' => '92135bf41f2b19a8ebd80902989ebc1f',
+));
---
+++ src/.rector-cache/fe/96/fe964f9b5c8fb427e39b9afe7343209a6c58e7c8.php
@@ -0,0 +1,6 @@
+<?php declare(strict_types = 1);
+
+return \Rector\Caching\ValueObject\CacheItem::__set_state(array(
+   'variableKey' => 'file_hash',
+   'data' => '3856f25ed676de3d9bbb543615d0fc66',
+));
