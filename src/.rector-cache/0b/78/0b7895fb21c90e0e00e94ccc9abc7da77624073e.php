e3058077680b4dbe8630cce7398b72bcd9a5261e
gassuncao <gassuncao@symplicity.com>
2025-08-17 21:30:59 -0300

    feat: Add CI pipeline for Laravel

---
+++ src/.rector-cache/0b/78/0b7895fb21c90e0e00e94ccc9abc7da77624073e.php
@@ -0,0 +1,6 @@
+<?php declare(strict_types = 1);
+
+return \Rector\Caching\ValueObject\CacheItem::__set_state(array(
+   'variableKey' => 'file_hash',
+   'data' => '158a49b88ad871d348cb1ebe1fa3e17e',
+));
---
+++ src/.rector-cache/a7/58/a758758c1067decaf7c4b02d9c14efbc510ca564.php
@@ -0,0 +1,6 @@
+<?php declare(strict_types = 1);
+
+return \Rector\Caching\ValueObject\CacheItem::__set_state(array(
+   'variableKey' => 'configuration_hash',
+   'data' => '979a12e5b3718a9ba4d8b428dd9e58678ac3a12c',
+));
