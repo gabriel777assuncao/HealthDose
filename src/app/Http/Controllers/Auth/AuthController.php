9fc446257eba3940bd54ec9300bccfaff61fe0eb
gabriel assunção <gabriel.costa@sga.pucminas.br>
2025-08-18 22:06:02 -0300

    feat: adding a user create route

--- src/app/Http/Controllers/Auth/AuthController.php
+++ src/app/Http/Controllers/Auth/AuthController.php
@@ -2,20 +2,25 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\AuthRequest;
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
 
-class AuthController
+class AuthController extends Controller
 {
+    public function __construct(private readonly User $user)
+    {
+    }
+
     public function register(AuthRequest $request): JsonResponse
     {
         Log::info('Chegou no controller!');
+        dd('aaaaaaaa');
 
-        $data = $request->validated();
-
-        dd($data);
-
-        return response()->json(['message' => 'test']);
+        $this->user->create($request->validated());
+        return response()->json(['message' => 'user created successfully', Response::HTTP_CREATED]);
     }
 }
